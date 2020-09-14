a7a39a2e817469d86c15e2568a810d2e8d1dcb49
s-plotnikov <seryoga.22@mail.ru>
2020-09-14 20:31:32 +0700

    added mobile

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,19 @@ $(function(){
 		autoScrolling:true,
         scrollHorizontally: true,
         sectionSelector: '.page-section',
-	});
+        scrollOverflow: true,
+        anchors: ['top', 'products', 'benefits', 'specification', 'questions', 'contacts'],
+        menu: '#header__nav',
+    });
+    
+    $('.menu__btn').on('click', function(){
+        $('.menu__btn').toggleClass('menu__btn--active');
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
+    $('.menu__list-link').on('click', function(){
+        $('.menu__btn').removeClass('menu__btn--active');
+        $('.menu__list').removeClass('menu__list--active');
+    });
   
 });
